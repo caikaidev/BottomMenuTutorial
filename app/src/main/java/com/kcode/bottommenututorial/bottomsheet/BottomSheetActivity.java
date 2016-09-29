510ad5f8e6fa898c59c8a15ecc2ae195af0b8785
fccaikai <714564390@qq.com>
2016-09-29 09:30:08 +0800

    添加Bottom Sheet 实现底部菜单

---
+++ app/src/main/java/com/kcode/bottommenututorial/bottomsheet/BottomSheetActivity.java
@@ -0,0 +1,13 @@
+package com.kcode.bottommenututorial.bottomsheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BottomSheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet);
+    }
+}
---
+++ app/src/main/java/com/kcode/bottommenututorial/bottomsheet/BottomSheetFragment.java
@@ -0,0 +1,10 @@
+package com.kcode.bottommenututorial.bottomsheet;
+
+import android.support.design.widget.BottomSheetDialogFragment;
+
+/**
+ * Created by caik on 2016/9/28.
+ */
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+}
